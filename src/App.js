1c4613ead9c247d1e7a30c945486b3e890b311f3
lutacch <lu.tacchetti@gmail.com>
2023-01-10 15:43:01 +0000

    commit

--- src/App.js
+++ src/App.js
@@ -147,13 +147,13 @@ function App() {
           {movies.map((movie) => (
             <div
               key={movie.id}
-              className="col-md-4 mb-3"
+              className="col-md-4 mb-5"
               onClick={() => selectMovie(movie)}
             >
               <img
                 src={`${URL_IMAGE + movie.poster_path}`}
                 alt=""
-                height={600}
+                height={"100%"}
                 width={"100%"}
               />
               <h4 className="text-center">{movie.title}</h4>
